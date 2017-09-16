b7ddd2ac3b529f1db692e2b74da717fc35479d55
Sjon Kosse <sjon@x61.eu>
2017-09-16 17:38:32 +0200

    Thumbnail extraction support. Will likely be changed to no longer with an image.Image but instead copy the raw bytes to a file.

--- arw.go
+++ arw.go
@@ -1,57 +1,59 @@
+//package arw implements basic support for Exif 2.3 according to CIPA DC-008-2012
 package arw
 
 import (
-	"io"
-	"errors"
 	"encoding/binary"
+	"errors"
 	"fmt"
-	"strings"
+	"io"
 	"strconv"
+	"strings"
+	"image"
+	"bytes"
+	"image/jpeg"
 )
 
-type metadata struct {
-	TIFFHeader
-	zeroth EXIFIFD
-}
-
+//CIPA DC-008-2012 Table 1
 type TIFFHeader struct {
 	ByteOrder uint16
-	FortyTwo uint16
-	Offset uint32
+	FortyTwo  uint16
+	Offset    uint32
 }
 
+//CIPA DC-008-2012 Chapter 4.6.2
 type EXIFIFD struct {
-	Count uint16
-	FIA []IFDFIA
+	Count   uint16
+	FIA     []IFDFIA
 	FIAvals []FIAval
-	Offset uint32
+	Offset  uint32
 }
 
 func (e EXIFIFD) String() string {
 	var result []string
-	result = append( result,fmt.Sprintf("Count: %v",e.Count))
+	result = append(result, fmt.Sprintf("Count: %v", e.Count))
 	for i := range e.FIA {
-		result = append(result,fmt.Sprintf("%+v",e.FIA[i]))
-		result = append(result,fmt.Sprintf("%+v",e.FIAvals[i]))
+		result = append(result, fmt.Sprintf("%+v", e.FIA[i]))
+		result = append(result, fmt.Sprintf("%+v", e.FIAvals[i]))
 	}
-	result = append(result,fmt.Sprintf("Offset to next EXIFIFD: %v",e.Offset))
-	return strings.Join(result,"\n")
+	result = append(result, fmt.Sprintf("Offset to next EXIFIFD: %v", e.Offset))
+	return strings.Join(result, "\n")
 }
 
+//CIPA DC-008-2012 Chapter 4.6.2
 type FIAval struct {
 	IFDtype
-	ascii *[]byte
-	short *[]uint16
-	long *[]uint32
-	slong *[]int32
-	longlong *[]uint64
+	ascii     *[]byte
+	short     *[]uint16
+	long      *[]uint32
+	slong     *[]int32
+	longlong  *[]uint64
 	slonglong *[]int64
 }
 
 func (f FIAval) String() string {
 	var val string
 	switch f.IFDtype {
-	case 1,2,7:
+	case 1, 2, 7:
 		val = fmt.Sprint(string(*f.ascii))
 	case 3:
 		val = fmt.Sprint(*f.short)
@@ -60,81 +62,58 @@ func (f FIAval) String() string {
 	case 9:
 		val = fmt.Sprint(*f.slong)
 	case 5:
-		for _,long := range *f.longlong {
-			val += fmt.Sprintf("%v.%v, ",(long>>32)&0xffffffff,long&0xffffffff)
+		for _, long := range *f.longlong {
+			val += fmt.Sprintf("%v.%v, ", (long>>32)&0xffffffff, long&0xffffffff)
 		}
 	case 10:
-		for _,long := range *f.slonglong {
-			val += fmt.Sprintf("%v.%v, ",(long>>32)&0xffffffff,long&0xffffffff)
+		for _, long := range *f.slonglong {
+			val += fmt.Sprintf("%v.%v, ", (long>>32)&0xffffffff, long&0xffffffff)
 		}
 	}
 
-	return f.IFDtype.String()+": "+val
+	return f.IFDtype.String() + ": " + val
 }
 
+//go:generate stringer -type=IFDtag
 type IFDtag uint16
 
-func (i IFDtag) String() string {
-	var r string
-	switch i {
-	case 254:
-		r = "Exif.Image.NewSubfileType"
-	case 259:
-		r= "Exif.Image.Compression"
-	case 270:
-		r = "Exif.Image.ImageDescription"
-	case 271:
-		r = "Exif.Image.Make"
-	case 272:
-		r = "Exif.Image.Model"
-	case 274:
-		r = "Exif.Image.Orientation"
-	case 282:
-		r = "Exif.Image.XResolution"
-	case 283:
-		r = "Exif.Image.YResolution"
-	case 296:
-		r = "Exif.Image.ResolutionUnit"
-	case 305:
-		r = "Exif.Image.Software"
-	case 306:
-		r = "Exif.Image.DateTime"
-	case 330:
-		r = "Exif.Image.SubIFDs"
-	case 513:
-		r = "Exif.Image.JPEGInterchangeFormat"
-	case 514:
-		r = "Exif.Image.JPEGInterchangeFormatLength"
-	case 531:
-		r = "Exif.Image.YCbCrPositioning"
-	case 34665:
-		r = "Exif.Image.ExifTag"
-	case 34853:
-		r = "Exif.Image.GPSTag"
-	case 40965:
-		r = "Exif.Photo.InteroperabilityTag"
-	case 50341:
-		r = "Exif.Image.PrintImageMatching"
-	case 50740:
-		r = "Exif.Image.DNGPrivateData"
-	default:
-		r = strconv.Itoa(int(i))
-	}
-	return r
-}
+//IFDtags mapping taken from http://www.exiv2.org/tags.html
+const (
+	NewSubFileType              IFDtag = 254
+	Compression                 IFDtag = 259
+	ImageDescription            IFDtag = 270
+	Make                        IFDtag = 271
+	Model                       IFDtag = 272
+	Orientation                 IFDtag = 274
+	XResolution                 IFDtag = 282
+	YResolution                 IFDtag = 283
+	ResolutionUnit              IFDtag = 296
+	Software                    IFDtag = 305
+	DateTime                    IFDtag = 306
+	SubIFDs                     IFDtag = 330
+	JPEGInterchangeFormat       IFDtag = 513
+	JPEGInterchangeFormatLength IFDtag = 514
+	YCbCrPositioning            IFDtag = 531
+	ExifTag                     IFDtag = 34665
+	GPSTag                      IFDtag = 34853
+	InteroperabilityTag         IFDtag = 40965
+	PrintImageMatching          IFDtag = 50341
+	DNGPrivateData              IFDtag = 50740
+)
 
+//IFD datatype, most datatypes translate in to C datatypes.
 type IFDtype uint16
 
-//Length of bytes
+//IFDType length in bytes
 func (i IFDtype) Len() int {
 	switch i {
-	case 1,2,7:
+	case 1, 2, 7:
 		return 1
 	case 3:
 		return 2
-	case 4,9:
+	case 4, 9:
 		return 4
-	case 5,10:
+	case 5, 10:
 		return 8
 	default:
 		panic("Unknown IFDtype")
@@ -145,40 +124,42 @@ func (i IFDtype) String() string {
 	var r string
 	switch i {
 	case 1:
-		r= "BYTE"
+		r = "BYTE"
 	case 2:
-		r= "ASCII"
+		r = "ASCII"
 	case 3:
-		r= "SHORT"
+		r = "SHORT"
 	case 4:
-		r= "LONG"
+		r = "LONG"
 	case 5:
-		r= "RATIONAL"
+		r = "RATIONAL"
 	case 7:
-		r= "UNDEFINED"
+		r = "UNDEFINED"
 	case 9:
-		r= "SLONG"
+		r = "SLONG"
 	case 10:
-		r= "SRATIONAL"
+		r = "SRATIONAL"
 	default:
-		panic("Unknown IFDtype: "+strconv.Itoa(int(i)))
-		}
+		panic("Unknown IFDtype: " + strconv.Itoa(int(i)))
+	}
 	return r
 }
 
 //IFD Field Interoperability Array
+//CIPA DC-008-2012 Chapter 4.6.2
 type IFDFIA struct {
-	Tag IFDtag
-	Type IFDtype
-	Count uint32
+	Tag    IFDtag
+	Type   IFDtype
+	Count  uint32
 	Offset uint32
 }
 
 //Anyone who thinks I'm switching byte order mid program is sorely mistaken.
 var b binary.ByteOrder
 
-func extractMetaData(r io.ReadSeeker) (m metadata,err error) {
-	endian := make([]byte,2)
+//ExtractMetadata will return the first IFD from a TIFF document.
+func ExtractMetaData(r io.ReadSeeker) (meta EXIFIFD, err error) {
+	endian := make([]byte, 2)
 	r.Read(endian)
 	switch string(endian) {
 	case "II":
@@ -186,73 +167,72 @@ func extractMetaData(r io.ReadSeeker) (m metadata,err error) {
 	case "MM":
 		b = binary.BigEndian
 	default:
-		return m,errors.New("failed to determine endianness, unsupposed reader")
+		return meta, errors.New("failed to determine endianness")
 	}
-	r.Seek(0,0)
+	r.Seek(0, 0)
 
 	var header TIFFHeader
-	binary.Read(r,b,&header)
-	r.Seek(int64(header.Offset),0)
-	m.TIFFHeader = header
+	binary.Read(r, b, &header)
+	r.Seek(int64(header.Offset), 0)
 
-	binary.Read(r,b,&m.zeroth.Count)
-	m.zeroth.FIA = make([]IFDFIA,int(m.zeroth.Count))
-	binary.Read(r,b,&m.zeroth.FIA)
-	binary.Read(r,b,&m.zeroth.Offset)
+	binary.Read(r, b, &meta.Count)
+	meta.FIA = make([]IFDFIA, int(meta.Count))
+	binary.Read(r, b, &meta.FIA)
+	binary.Read(r, b, &meta.Offset)
 
-	m.zeroth.FIAvals = make([]FIAval,len(m.zeroth.FIA))
-	for n,interop := range m.zeroth.FIA {
-		m.zeroth.FIAvals[n].IFDtype = interop.Type
+	meta.FIAvals = make([]FIAval, len(meta.FIA))
+	for n, interop := range meta.FIA {
+		meta.FIAvals[n].IFDtype = interop.Type
 
 		//Offset field is actually the value
 		if uint32(interop.Type.Len())*interop.Count <= 4 {
 			switch interop.Type {
-			case 1,2,7:
-				values := make([]byte,interop.Count)
+			case 1, 2, 7:
+				values := make([]byte, interop.Count)
 				for i := range values {
-					values[i] = byte(((interop.Offset << uint32(8*i)) & 0xff000000)>>24)
+					values[i] = byte(((interop.Offset << uint32(8*i)) & 0xff000000) >> 24)
 				}
-				m.zeroth.FIAvals[n].ascii = &values
+				meta.FIAvals[n].ascii = &values
 			case 3:
-				values := make([]uint16,interop.Count)
+				values := make([]uint16, interop.Count)
 				for i := range values {
-					values[i] = uint16(((interop.Offset << uint32(16*i)) & 0xffff0000)>>16)
+					values[i] = uint16(((interop.Offset << uint32(16*i)) & 0xffff0000) >> 16)
 				}
-				m.zeroth.FIAvals[n].short = &values
+				meta.FIAvals[n].short = &values
 			case 4:
 				values := []uint32{interop.Count}
-				m.zeroth.FIAvals[n].long = &values
+				meta.FIAvals[n].long = &values
 			case 9:
 				values := []int32{int32(interop.Count)}
-				m.zeroth.FIAvals[n].slong = &values
+				meta.FIAvals[n].slong = &values
 			}
 		} else {
-			r.Seek(int64(interop.Offset),0)
+			r.Seek(int64(interop.Offset), 0)
 			switch interop.Type {
 			case 1, 2, 7:
 				values := make([]byte, interop.Count)
-				binary.Read(r,b,&values)
-				m.zeroth.FIAvals[n].ascii = &values
+				binary.Read(r, b, &values)
+				meta.FIAvals[n].ascii = &values
 			case 3:
 				values := make([]uint16, interop.Count)
-				binary.Read(r,b,&values)
-				m.zeroth.FIAvals[n].short = &values
+				binary.Read(r, b, &values)
+				meta.FIAvals[n].short = &values
 			case 4:
 				values := make([]uint32, interop.Count)
-				binary.Read(r,b,&values)
-				m.zeroth.FIAvals[n].long = &values
+				binary.Read(r, b, &values)
+				meta.FIAvals[n].long = &values
 			case 9:
 				values := make([]int32, interop.Count)
-				binary.Read(r,b,&values)
-				m.zeroth.FIAvals[n].slong = &values
+				binary.Read(r, b, &values)
+				meta.FIAvals[n].slong = &values
 			case 5:
 				values := make([]uint64, interop.Count)
-				binary.Read(r,b,&values)
-				m.zeroth.FIAvals[n].longlong = &values
+				binary.Read(r, b, &values)
+				meta.FIAvals[n].longlong = &values
 			case 10:
 				values := make([]int64, interop.Count)
-				binary.Read(r,b,&values)
-				m.zeroth.FIAvals[n].slonglong = &values
+				binary.Read(r, b, &values)
+				meta.FIAvals[n].slonglong = &values
 			}
 
 		}
@@ -261,41 +241,57 @@ func extractMetaData(r io.ReadSeeker) (m metadata,err error) {
 	return
 }
 
+//ExtractThumbnail extracts an embedded JPEG thumbnail.
+//The extracted image.Image has no EXIF data included.
+func ExtractThumbnail(r io.ReaderAt,offset uint32,length uint32) (image.Image, error){
+	jpegData := make([]byte, length)
+	_,err := r.ReadAt(jpegData, int64(offset))
+	if err != nil {
+		return nil,err
+	}
+	reader := bytes.NewReader(jpegData)
+	jpg, err := jpeg.Decode(reader)
+	if err != nil {
+		return nil,err
+	}
+	return jpg,nil
+}
+
 //TODO(sjon): spec claims I should handle NULLs for ASCII
-func readByte(r io.Reader) byte{
+func readByte(r io.Reader) byte {
 	var bt byte
-	binary.Read(r,b,&bt)
+	binary.Read(r, b, &bt)
 	return bt
 }
 
-func readUint16(r io.Reader) uint16{
+func readUint16(r io.Reader) uint16 {
 	var short uint16
-	binary.Read(r,b,&short)
+	binary.Read(r, b, &short)
 	return short
 }
 
-func readUint32(r io.Reader) uint32{
+func readUint32(r io.Reader) uint32 {
 	var long uint32
-	binary.Read(r,b,&long)
+	binary.Read(r, b, &long)
 	return long
 }
 
 //Used for fixpoint of 32 bit numerator and denominator
-func readUint64(r io.Reader) uint64{
+func readUint64(r io.Reader) uint64 {
 	var longlong uint64
-	binary.Read(r,b,&longlong)
+	binary.Read(r, b, &longlong)
 	return longlong
 }
 
-func readInt32(r io.Reader) int32{
+func readInt32(r io.Reader) int32 {
 	var long int32
-	binary.Read(r,b,&long)
+	binary.Read(r, b, &long)
 	return long
 }
 
 //Used for fixpoint of 32 bit numerator and denominator
-func readInt64(r io.Reader) int64{
+func readInt64(r io.Reader) int64 {
 	var longlong int64
-	binary.Read(r,b,&longlong)
+	binary.Read(r, b, &longlong)
 	return longlong
 }
--- arw_test.go
+++ arw_test.go
@@ -1,27 +1,48 @@
 package arw
 
 import (
-	"testing"
+	"image/jpeg"
 	"os"
+	"testing"
+	"time"
+	"fmt"
 )
 
 const testFileLocation = "samples"
 
 func TestMetadata(t *testing.T) {
 	os.Chdir(testFileLocation)
-	//testJPG, err := os.Open("1.JPG")
-	//if err != nil {
-	//	t.Error(err)
-	//}
 	testARW, err := os.Open("1.ARW")
 	if err != nil {
 		t.Error(err)
 	}
 
-	meta,err := extractMetaData(testARW)
+	meta, err := ExtractMetaData(testARW)
 	if err != nil {
 		t.Error(err)
 	}
 
-	t.Log(meta.zeroth)
+	t.Log(meta)
+
+	var jpegOffset uint32
+	var jpegLength uint32
+	for i := range meta.FIA {
+		switch meta.FIA[i].Tag {
+		case JPEGInterchangeFormat:
+			jpegOffset = meta.FIA[i].Offset
+		case JPEGInterchangeFormatLength:
+			jpegLength = meta.FIA[i].Offset
+		}
+	}
+	jpg,err := ExtractThumbnail(testARW,jpegOffset,jpegLength)
+	if err != nil {
+		t.Error(err)
+	}
+
+	out,err := os.Create(fmt.Sprint(time.Now().Unix(),".jpg"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	jpeg.Encode(out,jpg,nil)
 }
