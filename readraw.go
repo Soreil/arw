ab65fa477dd4d229a5c810357dc07e94316c355f
Sjon Kosse <sjon@x61.eu>
2017-12-07 11:10:45 +0100

    Gammacorrection implemented
    The gamma correct seems to produce a correct result, the image is still
    missing significant amount of brightness when compared to the out of
    camera JPEG image.

--- readraw.go
+++ readraw.go
@@ -1,9 +1,12 @@
 package arw
 
 import (
+	"github.com/gonum/matrix/mat64"
 	"image"
 	"image/color"
 	"io"
+	"log"
+	"math"
 	"reflect"
 	"unsafe"
 )
@@ -84,27 +87,66 @@ func extractDetails(rs io.ReadSeeker) (rawDetails, error) {
 	return rw, nil
 }
 
-//The gamma curve points are in a 14 bit space space where we draw a curve that goes through the points.
-func gammacorrect(pix uint32, curve [4]uint32) uint32 {
-	const fifth = 0x3fff / 5
-	switch {
-	case pix <= fifth:
-		return (pix * 0x3fff) / curve[0]
-	case pix <= fifth*2:
-		return (pix * 0x3fff) / curve[1]
-	case pix <= fifth*3:
-		return (pix * 0x3fff) / curve[2]
-	case pix <= fifth*4:
-		return (pix * 0x3fff) / curve[3]
+//Helper function for gammacorrect
+func vandermonde(a []float64, degree int) *mat64.Dense {
+	x := mat64.NewDense(len(a), degree+1, nil)
+	for i := range a {
+		for j, p := 0, 1.; j <= degree; j, p = j+1, p*a[i] {
+			x.Set(i, j, p)
+		}
 	}
-	return pix
+	return x
 }
 
-func process(cur uint32, black uint32, gamma [4]uint32, whitebalance uint32) uint32 {
-	cur -= black
-	cur = (cur * whitebalance) / 1024
-	//cur = gammacorrect(cur,gamma)
+//This function is created by gammacorrect
+var gamma func(uint32) uint32
 
+//The gamma curve points are in a 14 bit space space where we draw a curve that goes through the points.
+func gammacorrect(curve [4]uint32) {
+	x := []float64{0, 1, 2, 3, 4, 5} // It would be nice if we could make this [0,0x3fff] but that seems to be impossible
+
+	y := []float64{0, float64(curve[0]), float64(curve[1]), float64(curve[2]), float64(curve[3]), 0x3fff}
+	const degree = 5
+
+	a := vandermonde(x, degree)
+	b := mat64.NewDense(len(y), 1, y)
+	c := mat64.NewDense(degree+1, 1, nil)
+
+	qr := new(mat64.QR)
+	qr.Factorize(a)
+
+	if err := c.SolveQR(qr, false, b); err != nil {
+		log.Println(err)
+	}
+
+	gamma = func(g uint32) uint32 {
+		if g > 0x3fff {
+			return 0x3fff //TODO(sjon): Should it be concidered a bug if we receive blown out values here?
+		}
+		x := (float64(g) / 0x3fff) * 5 //We need to keep x in between 0 and 5, this maps to 0x0 to 0x3fff
+		x5 := c.At(5, 0) * math.Pow(x, 5)
+		x4 := c.At(4, 0) * math.Pow(x, 4)
+		x3 := c.At(3, 0) * math.Pow(x, 3)
+		x2 := c.At(2, 0) * math.Pow(x, 2)
+		x1 := c.At(1, 0) * math.Pow(x, 1)
+		x0 := c.At(0, 0) * math.Pow(x, 0)
+		val := x5 + x4 + x3 + x2 + x1 + x0 //The negative signs are already in the numbers
+		if val > 0x3fff {
+			//panic("unexpectedly high gamma conversion result")
+		}
+		return uint32(val)
+	}
+}
+
+func process(cur uint32, black uint32, whitebalance uint32) uint32 {
+	if cur <= black {
+		return cur
+	} else {
+		cur -= black
+	}
+
+	cur = (cur * whitebalance) / 1024
+	cur = gamma(cur)
 	return cur
 }
 
@@ -122,28 +164,29 @@ func readraw14(buf []byte, rw rawDetails) *RGB14 {
 	gamma[1] = uint32(rw.gammaCurve[1])
 	gamma[2] = uint32(rw.gammaCurve[2])
 	gamma[3] = uint32(rw.gammaCurve[3])
+	gammacorrect(gamma)
 
 	for y := 0; y < img.Rect.Max.Y; y++ {
 		for x := 0; x < img.Rect.Max.X; x++ {
 			cur = uint32(data[y*img.Stride+x])
-			cur = process(cur, uint32(rw.blackLevel[0]), gamma, uint32(rw.WhiteBalance[0]))
+			cur = process(cur, uint32(rw.blackLevel[0]), uint32(rw.WhiteBalance[0]))
 			img.Pix[y*img.Stride+x].R = uint16(cur)
 			x++
 
 			cur = uint32(data[y*img.Stride+x])
-			cur = process(cur, uint32(rw.blackLevel[1]), gamma, uint32(rw.WhiteBalance[1]))
+			cur = process(cur, uint32(rw.blackLevel[1]), uint32(rw.WhiteBalance[1]))
 			img.Pix[y*img.Stride+x].G = uint16(cur)
 		}
 		y++
 
 		for x := 0; x < img.Rect.Max.X; x++ {
 			cur = uint32(data[y*img.Stride+x])
-			cur = process(cur, uint32(rw.blackLevel[2]), gamma, uint32(rw.WhiteBalance[2]))
+			cur = process(cur, uint32(rw.blackLevel[2]), uint32(rw.WhiteBalance[2]))
 			img.Pix[y*img.Stride+x].G = uint16(cur)
 			x++
 
 			cur = uint32(data[y*img.Stride+x])
-			cur = process(cur, uint32(rw.blackLevel[3]), gamma, uint32(rw.WhiteBalance[3]))
+			cur = process(cur, uint32(rw.blackLevel[3]), uint32(rw.WhiteBalance[3]))
 			img.Pix[y*img.Stride+x].B = uint16(cur)
 		}
 	}
